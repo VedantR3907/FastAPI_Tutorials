97e41896f192edc8996234763ac4806efb11d1d3
VedantR3907 <vedant.temp.work@gmail.com>
2024-07-23 15:54:35 +0530

    Default value quert parameter

--- query_parameters.py
+++ query_parameters.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from typing import List, Optional
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -31,9 +32,18 @@ async def get_users(name: Optional[str] = Query(None), age: Optional[int] = Quer
         raise HTTPException(status_code=404, detail="No users found with the specified criteria")
 
     return results
+    #To hit the API use the following code command: -  
+    #http://127.0.0.1:8000/users/?age=25
+    #http://127.0.0.1:8000/users/?name=john
+    #http://127.0.0.1:8000/users/?age=30&name=john
 
-#To hit the API use the following code command: - 
-''' http://127.0.0.1:8000/users/?age=25
-    http://127.0.0.1:8000/users/?name=john
-    http://127.0.0.1:8000/users/?age=30&name=john
-'''
+
+
+#Query Parameter with Default Value
+@app.get("/get_johnDoe")
+async def get_johnDoe(name: Optional[str] = Query(default='John Doe')):
+    for existing_user in database:
+        if existing_user["name"] == name:
+            return JSONResponse(status_code=200, content=existing_user)
+    
+    raise HTTPException(status_code=404, detail = "No User Found")
